fb1ef50efc72a4cbd0c1fa3bab65e9967e8ee6bd
Erwan <erwan.a.marechal@gmail.com>
2025-03-13 09:03:02 +0100

    Add HomeView and ProjectsView components with initial templates

---
+++ portfolio/src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="text-center p-10">
+    <h1 class="text-4xl font-bold">Bienvenue sur mon portfolio</h1>
+    <p class="text-lg mt-2">Découvrez mes projets et mon parcours.</p>
+  </div>
+</template>
---
+++ portfolio/src/views/ProjectsView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="text-center p-10">
+    <h1 class="text-4xl font-bold">Mes Projets</h1>
+    <p class="text-lg mt-2">Liste de mes réalisations.</p>
+  </div>
+</template>
