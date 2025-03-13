a36d395bb41d6330c1057d1cd5f1658fdf3020ff
Erwan <erwan.a.marechal@gmail.com>
2025-03-13 09:29:12 +0100

    Add Tailwind CSS integration and update router configuration

--- portfolio/src/router/index.js
+++ portfolio/src/router/index.js
@@ -1,18 +1,18 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "@/views/HomeView.vue";
-import ProjectsView from "@/views/ProjectsView.vue";
-import AboutView from "@/views/AboutView.vue";
-import ContactView from "@/views/ContactView.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '@/views/HomeView.vue';
+import ProjectsView from '@/views/ProjectsView.vue';
+import AboutView from '@/views/AboutView.vue';
+import ContactView from '@/views/ContactView.vue';
 
 const routes = [
-  { path: "/", component: HomeView },
-  { path: "/projects", component: ProjectsView },
-  { path: "/about", component: AboutView },
-  { path: "/contact", component: ContactView },
+  { path: '/', component: HomeView },
+  { path: '/projects', component: ProjectsView },
+  { path: '/about', component: AboutView },
+  { path: '/contact', component: ContactView },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
 
